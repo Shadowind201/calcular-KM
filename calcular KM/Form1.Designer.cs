3064c34dcef1b570d8580d3e3764b4fb0e6c85f4
Shadowind201 <enzocarnier@gmail.com>
2024-06-06 22:15:26 -0300

    Adicionar arquivos de projeto.

---
+++ calcular KM/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace calcular_KM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtV = new System.Windows.Forms.TextBox();
+            this.txtT = new System.Windows.Forms.TextBox();
+            this.txtE = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblE = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tempo de viagem:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(29, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Velocidade média:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(29, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Eficência do carro:";
+            // 
+            // txtV
+            // 
+            this.txtV.Location = new System.Drawing.Point(155, 27);
+            this.txtV.Name = "txtV";
+            this.txtV.Size = new System.Drawing.Size(59, 20);
+            this.txtV.TabIndex = 3;
+            // 
+            // txtT
+            // 
+            this.txtT.Location = new System.Drawing.Point(155, 65);
+            this.txtT.Name = "txtT";
+            this.txtT.Size = new System.Drawing.Size(59, 20);
+            this.txtT.TabIndex = 4;
+            // 
+            // txtE
+            // 
+            this.txtE.Location = new System.Drawing.Point(155, 110);
+            this.txtE.Name = "txtE";
+            this.txtE.Size = new System.Drawing.Size(59, 20);
+            this.txtE.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnCalcular.ForeColor = System.Drawing.Color.Black;
+            this.btnCalcular.Location = new System.Drawing.Point(32, 155);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(94, 32);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblE.Location = new System.Drawing.Point(32, 210);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(0, 13);
+            this.lblE.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(274, 279);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtE);
+            this.Controls.Add(this.txtT);
+            this.Controls.Add(this.txtV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtV;
+        private System.Windows.Forms.TextBox txtT;
+        private System.Windows.Forms.TextBox txtE;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblE;
+    }
+}
+
---
+++ calcular KM/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calcular_KM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int V = Convert.ToInt32(txtV.Text);
+            int T = Convert.ToInt32(txtT.Text);
+            int E = Convert.ToInt32(txtE.Text);
+
+            double D = (V * T) / E;
+            lblE.Text = "Eficiencia por Km é: " + D.ToString();
+        }
+    }
+}
